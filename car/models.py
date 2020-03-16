96384152a0b93e4e6f5be7c3a2a1ef7dea520470
Francis Tsang <frtsang40@gmail.com>
2020-03-16 15:01:39 -0700

    changes to models

--- car/models.py
+++ car/models.py
@@ -1,3 +1,14 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    pub_date = models.DateTimeField('date published')
+    title = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.title
+
+    def publish_date(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- car/views.py
+++ car/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_posts_list = Post.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('car/index.html')
+    context = {
+        'latest_posts_list' : latest_posts_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, title_id):
+    return HttpResponse("Title is %s." % title_id)
+
+def results(request, title_id):
+    response = "looking at results for %s"
+    return HttpResponse(response % title_id)
+
+def vote(request, title_id):
+    return HttpResponse("test for %s." % title_id)
