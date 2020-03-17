cfd957a86781ea917e0863e64840e6ca9d8b2387
Francis Tsang <frtsang40@gmail.com>
2020-03-17 14:26:40 -0700

    fixing up more errors

--- car/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from car.models import Post
-# Register your models here.
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'created', 'modified')
-
-
-admin.site.register(Post, PostAdmin)
--- car/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'car'
-urlpatterns = [
-    path('', views.PostList.as_view(), name='post-list'),
-    path('<slug>/', views.PostDetailView.as_view(), name='post-detail')
-]
